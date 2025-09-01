ecaf258cb4b8b02a4f93964d52632a7acfc79fc9
TheSaurojit <saurojitkarmakar947@gmail.com>
2025-09-01 18:44:28 +0530

    videos:

---
+++ src/services/videos.ts
@@ -0,0 +1,38 @@
+import { FirestoreService } from "@/firebase/firestoreService"
+
+
+type VideosFetch = {
+    id : string ;
+    imageUrl: string;
+    videoUrl: string;
+}
+
+type VideoCreate = {
+    image: File;
+    videoUrl: string;
+}
+
+export async function fetchVideos() {
+
+    const videos = await FirestoreService.getAllDocs(`Videos`) as VideosFetch[]
+
+    return videos
+}
+
+
+export async function createVideo({ image , videoUrl } : VideoCreate) {
+
+     await FirestoreService.addDoc("Videos",{
+        imageUrl : await FirestoreService.uploadFile(image , "Video") ,
+        videoUrl : videoUrl
+    }) 
+
+    
+}
+
+
+export async function deleteVideo(docId : string) {
+
+     await FirestoreService.deleteDoc("Videos",docId) 
+    
+}
